7c7b1431d07e30215efafc67a834d0da18c65cb2
John Doe <loveless>
2024-07-26 19:52:17 +0545

    :python code

--- class.py
+++ class.py
@@ -7,6 +7,8 @@ class student:
      
     def getinfo(self) : #use of the self in the python.
         print(f"the language is{self.level}")
+    
+    @staticmethod #the use of static is that ke it ensure that if the function do not use the data member of class we use staic method
     def getgret(self) :
         print("good morning")
         
---
+++ constructor.py
@@ -0,0 +1,27 @@
+class student:
+    name="loveless"
+    age=10
+    level="Bachleor"
+
+     
+     
+    def getinfo(self) : #use of the self in the python.
+        print(f"the language is{self.level}")
+    
+    @staticmethod #the use of static is that ke it ensure that if the function do not use the data member of class we use staic method
+    def getgret(self) :
+        print("good morning")
+    def __init__(self):
+        print("i am creating a object")
+        
+     
+
+  
+   
+c= student()
+c.name="sushil"
+print(c.name)
+c.getinfo()
+c.getgret()
+
+    
