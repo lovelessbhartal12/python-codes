d85407d6b8789a19d052863422931e584a7d262b
John Doe <loveless>
2024-07-26 19:40:03 +0545

    class file is added

---
+++ class.py
@@ -0,0 +1,23 @@
+class student:
+    name="loveless"
+    age=10
+    level="Bachleor"
+
+     
+     
+    def getinfo(self) : #use of the self in the python.
+        print(f"the language is{self.level}")
+    def getgret(self) :
+        print("good morning")
+        
+     
+
+  
+   
+c= student()
+c.name="sushil"
+print(c.name)
+c.getinfo()
+c.getgret()
+
+    
